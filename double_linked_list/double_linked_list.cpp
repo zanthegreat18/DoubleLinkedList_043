3a37d925fd6c638a7865b7c241d1f0a8b013ad9a
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-25 10:18:09 +0700

    commit 2

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -9,3 +9,16 @@ public:
 	Node* next;
 	Node* prev;
 };
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo);
+	bool ListEmpty();
+	void ascending();
+	void descending();
+};
