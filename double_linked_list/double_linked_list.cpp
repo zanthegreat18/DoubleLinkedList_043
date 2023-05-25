518a0c0c206fe342f4d136a5464f80ae3528e35e
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-25 10:53:40 +0700

    commit 9

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -44,6 +44,13 @@ void DoubleLinkedList::addNode() {
 	if (START == NULL || nim <= START->noMhs) {
 		if (START != NULL && nim == START->noMhs) {
 			cout << "\nDuplicate number not allowed" << endl;
+			return;
 		}
+		newNode->prev = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
 	}
 }
